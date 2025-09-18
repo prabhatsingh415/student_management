26ad48ad97d84c47698758ad7ae3435bd2e989e5
prabhatsingh415 <prabhatsinghrajput795@gmail.com>
2025-09-18 13:44:07 +0530

    fixed the key increment issue

--- Backend/src/main/java/com/prabhat/StudentManagement/entity/Course.java
+++ Backend/src/main/java/com/prabhat/StudentManagement/entity/Course.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class Course {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long courseId;
 
     @Column(unique = true)
